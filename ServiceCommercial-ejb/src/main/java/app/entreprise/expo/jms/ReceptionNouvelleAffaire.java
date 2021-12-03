0e3ae87b70e4294af439bd44509bc5a90f1b9467
aulloscar <49066562+aulloscar@users.noreply.github.com>
2021-12-03 11:02:12 +0100

    Initial COmmit

---
+++ ServiceCommercial-ejb/src/main/java/app/entreprise/expo/jms/ReceptionNouvelleAffaire.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.entreprise.expo.jms;
+
+import app.entreprise.menuiserieshared.entities.Affaire;
+import app.entreprise.menuiserieshared.entities.RdvCommercial;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+/**
+ *
+ * @author Josselin
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "NouvelleAffaireFile")
+    ,
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class ReceptionNouvelleAffaire implements MessageListener {
+    
+    public ReceptionNouvelleAffaire() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        try {
+            if (message instanceof ObjectMessage) {
+                ObjectMessage om = (ObjectMessage) message;
+                Affaire affaire = (Affaire) om.getObject();
+                System.out.println("Reçu dans le service commercial: " + affaire.toString());
+                
+                // Creation d'un rdv commercial
+                System.out.println("Choix automatique d'une date de rdv : 02/12/2021 à 10h - Commercial 1");
+                RdvCommercial rdvCommercial = new RdvCommercial();
+                rdvCommercial.setIdRdv(1);
+                rdvCommercial.setDateRdv("02/12/2021");
+                rdvCommercial.setHeureRdv("10h");
+                rdvCommercial.setIdCommercial(1);
+                affaire.setRdvCommercial(rdvCommercial);
+                
+                // Enregistrement BD
+                
+                // Envoi de la confirmation du rdv
+                EnvoiAffaire envoiAffaire = new EnvoiAffaire();
+                envoiAffaire.send(affaire,"ConfirmationRdvFile");
+                
+            } else if (message != null) {
+                System.out.println("Received non object message");
+            } else {
+                System.out.println("???");
+            }
+        } catch(Exception ex){
+            System.out.println("Erreur lors de la réception : ");
+        }
+    }
+    
+}
